cc50f7e279952176bc545f11292d73954115f301
Yashi Vaish <yashi87vaish@gmail.com>
2024-12-22 20:47:20 +0530

    Update apiSlice.js

--- client/src/store/apiSlice.js
+++ client/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'https://expense-tracker-server-mosa.onrender.com';
+const baseURI = 'https://expense-tracker-server-fbei.onrender.com';
 
 export const apiSlice = createApi({
     baseQuery : fetchBaseQuery({ baseUrl : baseURI}),
