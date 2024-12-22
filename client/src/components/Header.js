c7ce5f4a19e53ecc8ab867b01f8ade91d1d9cdb6
yashi <yashi87vaish@gmail.com>
2024-12-22 17:44:21 +0530

    commit changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,59 +1,56 @@
-import React, { useState } from 'react'
-import moon from '../assets/moon.svg'
-import sun from '../assets/sun.svg'
-import useDarkMode from '../hooks/useDarkMode'
-import {motion} from 'framer-motion'
+import React, { useState } from "react";
+import moon from "../assets/moon.svg";
+import sun from "../assets/sun.svg";
+import useDarkMode from "../hooks/useDarkMode";
+import { motion } from "framer-motion";
 
 export default function Header() {
-    const [colorTheme , setTheme] = useDarkMode()
-    const [darkSide, setDarkSide] = useState(
-        colorTheme === 'light' ? true : false
-    )
+  const [colorTheme, setTheme] = useDarkMode();
+  const [setDarkSide] = useState(colorTheme === "light" ? true : false);
 
-    const toggleDarkMode = () => {
-        setTheme(colorTheme)
-        setDarkSide((state)=> !state)
-    }
+  const toggleDarkMode = () => {
+    setTheme(colorTheme);
+    setDarkSide((state) => !state);
+  };
 
-    const transition = {
-        type: 'spring',
-        stiffness: 200,
-        damping:10
-    }
+  const transition = {
+    type: "spring",
+    stiffness: 200,
+    damping: 10,
+  };
   return (
-        <div>
-          {/* Header */}
-          <header className='h-[80px] z-50 duration-300 ease-in-out p-4 bg-[#20504F] flex items-center justify-between'>
-              {/* Title */}
-              <h1 className="text-4xl text-white rounded shadow-lg p-4">Expense Tracker</h1>
+    <div>
+      {/* Header */}
+      <header className="h-[80px] z-50 duration-300 ease-in-out p-4 bg-[#20504F] flex items-center justify-between">
+        {/* Title */}
+        <h1 className="text-4xl text-white rounded shadow-lg p-4">
+          Expense Tracker
+        </h1>
 
-              {/* Right side */}
-                <div className='flex items-center'>
-
-                    {/* DarkMode Button */}
-                    {
-                        colorTheme === 'light' ?
-                            <motion.img 
-                                initial={{scale: 0.6, rotate: 90}}
-                                animate={{scale: 1,rotate: 360,transition}}
-                                whileTap={{scale: 0.9, rotate :15}}
-                                onClick={toggleDarkMode}
-                                src={moon}
-                                className=' cursor-pointer ml-8 h-10' />
-                            :
-                            <motion.img
-                                initial={{rotate: 45}}
-                                animate={{scale: 1,rotate: 360,transition}}
-                                whileTap={{scale: 0.9, rotate :15}}
-                                onClick={toggleDarkMode}
-                                src={sun}
-                                className=' cursor-pointer ml-8 h-10' />
-                    }
-                </div>
-          </header>
-          
+        {/* Right side */}
+        <div className="flex items-center">
+          {/* DarkMode Button */}
+          {colorTheme === "light" ? (
+            <motion.img
+              initial={{ scale: 0.6, rotate: 90 }}
+              animate={{ scale: 1, rotate: 360, transition }}
+              whileTap={{ scale: 0.9, rotate: 15 }}
+              onClick={toggleDarkMode}
+              src={moon}
+              className=" cursor-pointer ml-8 h-10"
+            />
+          ) : (
+            <motion.img
+              initial={{ rotate: 45 }}
+              animate={{ scale: 1, rotate: 360, transition }}
+              whileTap={{ scale: 0.9, rotate: 15 }}
+              onClick={toggleDarkMode}
+              src={sun}
+              className=" cursor-pointer ml-8 h-10"
+            />
+          )}
         </div>
-  )
+      </header>
+    </div>
+  );
 }
-
-
--- client/src/hooks/useDarkMode.js
+++ client/src/hooks/useDarkMode.js
@@ -1,20 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export default function useDarkMode() {
-    const [theme, setTheme] = useState(localStorage.theme)
-    
-    const colorTheme = theme === 'dark' ? 'light' : 'dark'
-    
-    useEffect(() => {
-        const root = window.document.documentElement
-        root.classList.remove(colorTheme)
-        root.classList.add(theme)
-        localStorage.setItem('theme', theme)
-      
-    }, [theme, colorTheme
-    ]);
+  const [theme, setTheme] = useState(localStorage.theme);
 
-    return[ colorTheme, setTheme]
-    
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme, colorTheme]);
+
+  return [colorTheme, setTheme];
 }
-
